5d9c7e0f6ee64d0adf1c1bf841a0c4a836bcadd5
Wagner Pereira <wagner_w@live.com>
2024-03-20 13:24:02 -0300

    atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import { turboLogger } from "./turboLogger.js";
 console.log("Estou usando o Github");
 
 turboLogger("estou aprendendo o código do git");
+turboLogger(
+  "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema",
+  "darkgreen"
+);
